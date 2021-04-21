b444155b519bb797e53d03de18cd6929bab489fb
Daniel Carmona <carmona240101@hotmail.com>
2021-04-21 10:51:26 +0100

    obter mensagem e mostar atividade/mensagem ao clicar no botao

--- app/src/main/java/pt/ipg/mensagem/MainActivity.kt
+++ app/src/main/java/pt/ipg/mensagem/MainActivity.kt
@@ -1,8 +1,10 @@
 package pt.ipg.mensagem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,12 @@ class MainActivity : AppCompatActivity() {
 
 
     fun enviaMensagem(view: View) {
+        val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
+        val mensagem = editTextMensagem.text.toString()
 
+        val intent = Intent(this, MostraMensagemActivity::class.java)
+
+
+        startActivity(intent)
     }
 }
