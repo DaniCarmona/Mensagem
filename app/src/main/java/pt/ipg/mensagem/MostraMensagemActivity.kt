aa07617a83112dc0072d6ec25b11a64c4e65d901
Daniel Carmona <carmona240101@hotmail.com>
2021-04-28 09:39:26 +0100

    data formatada pra ser só hora

--- app/src/main/java/pt/ipg/mensagem/MostraMensagemActivity.kt
+++ app/src/main/java/pt/ipg/mensagem/MostraMensagemActivity.kt
@@ -3,6 +3,7 @@ package pt.ipg.mensagem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MostraMensagemActivity : AppCompatActivity() {
@@ -14,7 +15,11 @@ class MostraMensagemActivity : AppCompatActivity() {
         val data : Date = intent.getSerializableExtra(MainActivity.INFO_EXTRA_DATA) as Date
         val textViewMensagem = findViewById<TextView>(R.id.editTextMensagem)
         textViewMensagem.setText(mensagem)
+
+        //Mostrar Data
         findViewById<TextView>(R.id.data)
+        SimpleDateFormat("HH:mm:ss")
+
         textViewMensagem.setText(data.toString())
     }
 }
